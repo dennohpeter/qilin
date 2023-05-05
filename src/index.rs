e74ff6750a4d530a56bacbdd7cf34bf6e3efabf9
FudgyDRS <yetimonsters@gmail.com>
2023-05-05 11:27:35 +0900

    sepolia monitor now working
    Co-authored-by: 野良犬 <da-bao-jian@users.noreply.github.com>

--- src/index.rs
+++ src/index.rs
@@ -1,9 +1,10 @@
 use dotenv::dotenv;
 use std::env;
-use ethers::core::{rand::thread_rng, types::transaction::eip2718::TypedTransaction};
+use ethers::core::{rand::thread_rng, types::TransactionRequest, types::transaction::eip2718::TypedTransaction};
 use ethers::prelude::*;
+use ethers::providers::{Provider, Ws};
 use ethers::core::k256::SecretKey;
-use ethers_flashbots::*;
+use ethers_flashbots::{BundleRequest, FlashbotsMiddleware};
 use eyre::Result;
 use std::convert::TryFrom;
 use url::Url;
@@ -11,53 +12,73 @@ use ethers_signers::{LocalWallet, Signer};
 
 #[tokio::main]
 pub async fn first_fn() -> Result<()> {
+    // data collection
     dotenv().ok(); 
-    let provider = Provider::<Http>::try_from("https://mainnet.eth.aragon.network")?;
+    let _infura_key = env::var("INFURA_SEPOLIA_API_KEY").clone().unwrap();
+    //let http_provider = Provider::try_from("https://mainnet.eth.aragon.network")?;
+    let http_provider = Provider::try_from(format!("https://sepolia.infura.io/v3/{}", _infura_key))?;
 
+    // see: https://www.gakonst.com/ethers-rs/providers/ws.html
+    let ws_url = format!("wss://sepolia.infura.io/ws/v3/{}", _infura_key);
+    let ws_provider = Provider::<Ws>::connect(ws_url).await?;
+
+    // bundle signing
     let _bundle_signer = env::var("FLASHBOTS_IDENTIFIER").clone().unwrap();
     let bundle_signer = _bundle_signer.parse::<LocalWallet>();
+    //let addr = _bundle_signer.address();
 
     let _wallet = env::var("FLASHBOTS_SIGNER").clone().unwrap();
-    let wallet = _wallet.parse::<LocalWallet>();
+    let wallet = _wallet.parse::<LocalWallet>().unwrap();
 
-    println!("Hello, world!");
     println!("Bundle Signer: {}", _bundle_signer);
     println!("Wallet: {}", _wallet);
 
     let client = SignerMiddleware::new(
         FlashbotsMiddleware::new(
-            provider,
-            Url::parse("https://relay.flashbots.net")?,
+            http_provider,
+            // use testnet builder
+            Url::parse("https://relay-sepolia.flashbots.net")?,
+            // Url::parse("https://relay.flashbots.net")?,
             bundle_signer.unwrap(),
         ),
-        wallet.unwrap(),
+        wallet,
     );
 
-    //println!("{}", client);
+    // let seploia_endpoint = "https://api-sepolia.etherscan.io/";
+//     https://api-sepolia.etherscan.io/api
+//    ?module=transaction
+//    &action=getstatus
+//    &txhash=0x5d329954fae7d19b2fb9abf0e6862735243b1079c58e0ea307d7e933657ac083
+//    &apikey=YourApiKeyToken
+    let mut stream = ws_provider.subscribe_pending_txs().await?;
 
-    // get last block number
-    let block_number = client.get_block_number().await?;
-    println!("Block Number: {}", block_number);
-
-    // Build a custom bundle that pays 0x0000000000000000000000000000000000000000
-    let tx = {
-        let mut inner: TypedTransaction = TransactionRequest::pay(Address::zero(), 100).into();
-        client.fill_transaction(&mut inner, None).await?;
-        inner
-    };
     
+    //let mut stream = ws_provider.pending_bundle(msg).await?;
+    while let Some(msg) = stream.next().await {
+        let mut msg2 = ws_provider.get_transaction(msg).await?;
+        println!(
+            // "Timestamp: {:?}, block number: {} -> {:?}", 
+            // block.timestamp,
+            // block.number.unwrap(),
+            // block.hash.unwrap()
+            // msg
+            "{:?}", msg2
+        );
 
-    let signature = client.signer().sign_transaction(&tx).await?;
-    //println!("signature: {}", signature);
-    let bundle = BundleRequest::new()
-        .push_transaction(tx.rlp_signed(&signature))
-        .set_block(block_number + 1)
-        .set_simulation_block(block_number)
-        .set_simulation_timestamp(0);
+        // let tx: TypedTransaction = TransactionRequest::pay("vitalik.eth", 1).into();
+        // let signature = client.signer().sign_transaction(&tx).await?;
+        // let mut bundle = BundleRequest::new();
+        // bundle.add_transaction(
+        //     tx.rlp_signed(&signature)
+        // );
+    
+        //let bundle = bundle.set_block(block.number.unwrap()+1).set_simulation_block(block.number.unwrap()).set_simulation_timestamp(0);
 
-    // Simulate it
-    let simulated_bundle = client.inner().simulate_bundle(&bundle).await?;
-    //println!("Simulated bundle: {:?}", simulated_bundle);
+        //sending bundle
+        // let pending_bundle = client.inner().send_bundle(&bundle).await?;
+    }
+
+
 
     Ok(())
 }
